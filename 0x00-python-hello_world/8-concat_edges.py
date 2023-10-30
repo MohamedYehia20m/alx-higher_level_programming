fadf7b1de0c5cb4b05b54ce1d8e6f52f3b995055
MohamedYehia20m <medoyehia2001@gmail.com>
2023-10-30 18:23:09 +0200

    use different type of interpolation

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-middle = str[39:66] + " with Python"
-print(middle)
+middle = str[39:66]
+print(f"{middle} with Python")
