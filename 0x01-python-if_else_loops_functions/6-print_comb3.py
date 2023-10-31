ae43b537b4e0d3cadcf0fe4a23117c5931c76599
MohamedYehia20m <medoyehia2001@gmail.com>
2023-10-31 11:32:47 +0200

    use .format() in Task6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,6 @@ for i in range(100):
     if (i/10) > (i % 10):
         continue
     if i == 89:
-        print(f"{i:d}")
-    print(f"{i:02d}", end=", ")
+        print("{:d}".format(i))
+        continue
+    print("{:02d}".format(i), end=", ")
