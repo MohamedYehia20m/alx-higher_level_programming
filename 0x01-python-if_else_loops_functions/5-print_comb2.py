7d105151b15e8a13a9a8b99539990f303f8bffa0
MohamedYehia20m <medoyehia2001@gmail.com>
2023-10-31 11:22:11 +0200

    fix pycodestyle in Task5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(100):
     if i == 99:
-        print(f"{i:d}")
-    print(f"{i:02d}", end=", ")
+        print("{:d}".format(i))
+    print("{:02d}".format(i), end=", ")
