1e1663fa652b6cf9ba57b111413423622d6684f5
MohamedYehia20m <medoyehia2001@gmail.com>
2023-10-31 11:58:47 +0200

    add Task9

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    number2 = number % 10
+    if number < 0:
+        number2 -= 10
+        number2 *= -1
+    print("{:d}".format(number2), end="")
