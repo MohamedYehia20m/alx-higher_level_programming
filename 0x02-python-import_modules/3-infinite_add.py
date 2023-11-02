1e3175b9c66dfe6d378d419ca073f5511bb862ee
MohamedYehia20m <medoyehia2001@gmail.com>
2023-11-02 12:09:11 +0200

    add Task3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+def printsum():
+    x = len(sys.argv)
+    sum = 0
+    for i in range(1, x):
+        sum += int(sys.argv[i])
+    print('{:d}'.format(sum))
+
+if __name__ == "__main__":
+    printsum()
+
