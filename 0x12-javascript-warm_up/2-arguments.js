6111555544f2911a50d9b0ec2b9c800852f0f96f
MohamedYehia20m <medoyehia2001@gmail.com>
2024-02-12 18:06:31 +0200

    modify Task2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
 
 const { argv } = require('node:process');
-let counter = 0;
+let counter = argv.length;
 
-argv.forEach((val, index) => {
-  counter += 1;
-});
 (counter === 2) ? console.log('No argument') : console.log('Argument found');
