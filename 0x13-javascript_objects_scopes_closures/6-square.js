99d17fea57193bf1579f188c6554449f8389cba8
MohamedYehia20m <medoyehia2001@gmail.com>
2024-02-13 19:47:07 +0200

    add Task6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const MainSquare = require('./5-square.js');
+class Square extends MainSquare {
+  charPrint (c) {
+    const char = c || 'X';
+    for (let i = 0; i < this.width; i++) {
+      console.log(char.repeat(this.height));
+    }
+  }
+}
+
+module.exports = Square;
