8c6afaf0c401607403d6524b73d79f2d6ec1f6be
MohamedYehia20m <medoyehia2001@gmail.com>
2024-02-13 18:23:59 +0200

    fix Task2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if ((w > 0) && (h > 0)) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
       this.width = w;
       this.height = h;
-    } else {
-      this.width;
-      this.height;
-    }	    
+    }
   }
 }
 
